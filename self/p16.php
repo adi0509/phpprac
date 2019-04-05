4bd2061bd7e9e313d8696e59a213345910b270a7
Adarsh <41581721+adi0509@users.noreply.github.com>
2019-04-05 17:03:08 +0530

    Update p16.php

--- self/p16.php
+++ self/p16.php
@@ -1,39 +1,43 @@
-///WOrk in Progress
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Practical 16</title>
 </head>
 <body>
+	<h1>Practical 16</h1>
+	<p>Enter number of terms: </p>
 	<form action="" method="post">
 		<input type="number" name="n">
 		<input type="submit" name="btn">
 	</form>
-
+	<br><br>
+	OUTPUT:<br>
 	<?php
 		function fibo($a, $b, $c)
 		{
-			if($a==1)
+			if($a==0)
 			{
-				echo "1 ";
 				return 1;
 			}
-			elseif($a==2)
+			else 
 			{
-				echo "1 1 ";
-				return 1;
-			}
-			else
-			{
-				echo $a+$b;
+				echo $b+$c." ";
+				fibo($a-1, $c,  $b+$c);
 			}
 		}
-
 		if(isset($_POST['btn']))
 		{
 			$num = $_POST['n'];
-			fibo($num);
+			if($num<=0)
+			{
+				echo "Enter a positive number!!";
+			}
+			if($num>=1)
+				echo "0 ";
+			if($num>=2)
+				echo "1 ";
+			if($num>=3)
+				fibo($num-2, 0 , 1);
 		}
 	?>
 </body>
